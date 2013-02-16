30715923f9165a71ef4d3985ff98170b316954a3
Doug Feigelson <dougfeig@gmail.com>
2013-02-16 17:27:20 -0500

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='zinc',
-    version='0.1.0',
+    version='0.1.1',
     author='Eric Swanson',
     author_email='eswanson@alloscomp.com',
     packages=['zinc'],
